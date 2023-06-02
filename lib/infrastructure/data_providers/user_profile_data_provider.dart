8dd064780352f6fb616e6f17d9a976a43d994229
Abdulmelik619 <abdulmelikambw619@gmail.com>
2023-06-02 13:38:42 +0300

    d

--- lib/infrastructure/data_providers/user_profile_data_provider.dart
+++ lib/infrastructure/data_providers/user_profile_data_provider.dart
@@ -1,52 +1,64 @@
 import 'dart:convert';
 
 import 'package:http/http.dart' as http;
+import 'package:picstash/domain/entities/local_user_model.dart';
 import '../../domain/entities/user_profile/user_profile.dart';
 
-
 class UserProfileDataProvider {
-  static const String baseUrl = 'http://127.0.0.1:3000/post'; // Replace with your API base URL
+  static const String _baseUrl =
+      'http://localhost:3000/users'; // Replace with your API base URL
 
-  Future<UserProfile> fetchUserProfile(String userId) async {
-    final response = await http.get(Uri.parse('$baseUrl/users/$userId/profile'));
+  fetchUserProfile(String userEmail) async {
+    final http.Response response =
+        await http.post(Uri.parse("$_baseUrl/getUser"),
+            headers: <String, String>{"Content-Type": "application/json"},
+            body: jsonEncode({
+              "email": userEmail,
+            }));
 
-    if (response.statusCode == 200) {
-      return UserProfile.fromJson(jsonDecode(response.body));
+    if (response.statusCode == 201) {
+      final user = jsonDecode(response.body);
+      return user;
     } else {
       throw Exception('Failed to fetch user profile');
     }
   }
 
-  Future<void> updateUserProfile(UserProfile userProfile) async {
+  updateUserProfile(LocalUserModel userModel) async {
     // Implement the logic to update the user profile in the data source
     // Use the userProfile parameter to perform the update
     // Make an HTTP PUT request or any other appropriate method for updating the profile
     // Replace the following code with your actual implementation
 
-    final profileUrl = '$baseUrl/users/${userProfile.id}/profile';
-    final body = jsonEncode(userProfile.toJson());
+    final http.Response response =
+        await http.post(Uri.parse("$_baseUrl/updateProfile"),
+            headers: <String, String>{"Content-Type": "application/json"},
+            body: jsonEncode({
+              "email": userModel.email.toString(),
+              "Name": userModel.name,
+              "bio": userModel.bio,
+            }));
 
-    final response = await http.put(Uri.parse(profileUrl), body: body, headers: {
-      'Content-Type': 'application/json',
-    });
-
-    if (response.statusCode != 200) {
+    if (response.statusCode == 201) {
+      return UserProfile.fromJson(jsonDecode(response.body));
+    } else {
       throw Exception('Failed to update user profile');
     }
   }
 
-  Future<void> logout() async {
+  logout() async {
     // Implement the logic to perform logout actions
     // Make an HTTP POST request or any other appropriate method for performing logout
     // Replace the following code with your actual implementation
 
-    const logoutUrl = '$baseUrl/logout';
+    const logoutUrl = 'http://localhost:3000auth/logout';
 
     final response = await http.post(Uri.parse(logoutUrl));
 
-    if (response.statusCode != 200) {
+    if (response.statusCode == 201) {
+      return response.statusCode;
+    } else {
       throw Exception('Failed to perform logout');
     }
   }
 }
-
